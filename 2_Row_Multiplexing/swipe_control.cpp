e4da4ef78fa6577e388db82526dcc469957635ed
kch114 <kch114@uclive.ac.nz>
2018-09-05 17:10:58 +1200

    All directions working, still throwing nan error.

--- 2_Row_Multiplexing/swipe_control.cpp
+++ 2_Row_Multiplexing/swipe_control.cpp
@@ -30,6 +30,7 @@ State determine_state(circBuf_t* x_buff, circBuf_t* y_buff) {
 	
 	//populate y buffer and y_time buffer with datapoints, ignoring zeros.
 	size = 0;
+	memset(t_temp, 0, BUFF_SIZE); //reset temp time buffer to zero
 	while (y_time <= BUFF_SIZE) {
 		int temp = readCircBuf(y_buff);
 		if (temp != 0) {
@@ -42,48 +43,10 @@ State determine_state(circBuf_t* x_buff, circBuf_t* y_buff) {
 	
 	float y[size];
 	float y_t[size];
-	memset(t_temp, 0, BUFF_SIZE); //reset time buffer to zero
 	for (int i = 0; i < size; i++) {
 		y[i] = y_temp[i];
 		y_t[i] = t_temp[i];
 	}
-	
-	/* //read the circ buff into a local buffer
-	for (int i = 0; i < BUFF_SIZE; i++) {
-		x[i] = readCircBuf(x_buff);
-		y[i] = readCircBuf(y_buff);
-	} */
-	
-	/* //go through each quadrant and average
-	for (int i = Q_Zero; i <= Q_Three; i++) {
-		for (int j = i * QUAD_SIZE; j < (i+1) * BUFF_SIZE; j++) {
-			x_average[i] += readCircBuf(x_buff);
-			y_average[i] += readCircBuf(y_buff);
-		}
-		//note these will be truncated as not float buffer
-		x_average[i] = x_average[i] / QUAD_SIZE;
-		//Serial.println(x_average[i]);
-		y_average[i] = y_average[i] / QUAD_SIZE;
-		//Serial.println(y_average[i]);
-
-	} */
-	/* //iteratively move through the buffer until a swipe has been determined.
-	//if no swipe is selected, will default to waiting.
-	//TODO currently there is a priority on x swipes which is not ideal.
-	for (int i = Q_Zero; i <= Q_Three; i++) {
-		
-		//wraparound
-		int next = i + 1;
-		if (next == Q_Three + 1) {
-			next = Q_Zero;
-		}
-		int next2 = i + 2;
-		if (next2 == Q_Three + 1) {
-			next2 = Q_Zero;
-		} else if (next2 == Q_Three + 2) {
-			next2 = Q_One;
-		}
-	} */
 	
 	//use linear regression to determine gradient of swipe
 	float x_grad = simpLinReg(x_t, x, sizeof(x_t)/sizeof(float));
@@ -93,48 +56,20 @@ State determine_state(circBuf_t* x_buff, circBuf_t* y_buff) {
 	Serial.print("Y: ");
 	Serial.println(y_grad, 4);
 	
-	
-	if (x_grad > y_grad) {
+	//determine state
+	if (abs(x_grad) > abs(y_grad)) {
 		if (x_grad > 0) {
 			state = R_Swipe;
 		} else {
 			state = L_Swipe;
 		}
-	} else if (y_grad > x_grad) {
+	} else if (abs(y_grad) > abs(x_grad)) {
 		if (y_grad > 0) {
 			state = U_Swipe;
 		} else {
 			state = D_Swipe;
 		}
 	}
-		
-		/* //if x distance decreases over time (by one section per quadrant)
-		if (x_average[i] != 0) {
-			Serial.println(x_average[i]);
-			if (x_average[i] > x_average[next] + MIN_DISTANCE 
-							 && x_average[next] > x_average[next2] + MIN_DISTANCE) {
-				state = L_Swipe;
-				break;
-			}
-			//if x distance increases over time		
-			else if (x_average[i] + MIN_DISTANCE < x_average[next] 
-							 && x_average[next] + MIN_DISTANCE < x_average[next2]) {
-				state = R_Swipe;
-				break;
-			}
-		} else if (y_average[i] != 0) {
-			//if y distance decreases over time		
-			if (y_average[i] > y_average[next] + MIN_DISTANCE 
-							 && y_average[next] > y_average[next2] + MIN_DISTANCE) {
-				state = D_Swipe;
-				break;
-			}
-			//if y distance increases over time		
-			else if (y_average[i] + MIN_DISTANCE < y_average[next] 
-							 && y_average[next] + MIN_DISTANCE < y_average[next2]) {
-				state = U_Swipe;
-				break;
-			}
-		} */
+	
 	return state;
 }			
