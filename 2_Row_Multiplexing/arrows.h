7267c62fb4822db82cb72656f3c8c22f6c6d388b
elesmera11 <elesmera11@hotmail.com>
2018-09-11 12:29:50 +1200

    Update arrows.h

--- 2_Row_Multiplexing/arrows.h
+++ 2_Row_Multiplexing/arrows.h
@@ -1,4 +1,8 @@
-#define DOWN_1 {{0, 0, 1, 0, 0}, {0, 0, 1, 0, 0}, {0, 0, 1, 0, 0}, {0, 1, 1, 1, 0}, {0, 0, 1, 0, 0}}
+//header file for defining the LED bitmasks(kinda) of the animations.
+//each entry is the digital bitmask for a row that has analogue input set to active.
+//each matrix can be rotated (theoretically) by rotate_CW() in distance_LEDs.cpp
+//GLHF
+#define UP_1 {{0, 0, 1, 0, 0}, {0, 0, 1, 0, 0}, {0, 0, 1, 0, 0}, {0, 1, 1, 1, 0}, {0, 0, 1, 0, 0}}
 #define UP_2 {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 1, 1, 1, 0}} 
 #define UP_3 {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 1, 1, 1, 0}, {1, 1, 1, 1, 1}}
 #define UP_4 {{0, 0, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 1, 1, 1, 0}, {1, 1, 1, 1, 1}, {0, 1, 1, 1, 0}}
@@ -8,5 +12,7 @@
 #define UP_8 {{0, 1, 1, 1, 0}, {0, 1, 1, 1, 0}, {0, 1, 1, 1, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}}
 #define UP_9 {{0, 1, 1, 1, 0}, {0, 1, 1, 1, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}}
 #define UP_10 {{0, 1, 1, 1, 0},{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}}
-#define SERIES {DOWN_1, UP_2, UP_3,	UP_4,	UP_5, UP_6, UP_7, UP_8, UP_9,	UP_10}
+
+//define the sequence order and size.
+#define SERIES {UP_1, UP_2, UP_3,	UP_4,	UP_5, UP_6, UP_7, UP_8, UP_9,	UP_10}
 #define SERIES_SIZE 10
